137f40d84d69e44115b89f627c04fb62a9abf4e1
kuljeet007 <kuljeetsingh01588@gmail.com>
2026-07-10 12:34:49 +0530

    fix: resolve wishlist foreign key constraint conflict and support both property names

--- Sahakaar_API/Models/Masters/mCart.cs
+++ Sahakaar_API/Models/Masters/mCart.cs
@@ -6,8 +6,20 @@ namespace Sahakaar_API.Models.Masters
     {
         public decimal Id { get; set; } = 0;
 
+        private decimal _itemDesignId = 0;
+
         [Required(ErrorMessage = "ItemDesignId is required.")]
-        public decimal F_ItemDesignMaster { get; set; }
+        public decimal F_ItemDesignMaster
+        {
+            get => _itemDesignId;
+            set => _itemDesignId = value;
+        }
+
+        public decimal ItemDesignId
+        {
+            get => _itemDesignId;
+            set => _itemDesignId = value;
+        }
 
         [Required(ErrorMessage = "Qty is required.")]
         public decimal Qty { get; set; }
--- Sahakaar_API/Models/Masters/mWishlist.cs
+++ Sahakaar_API/Models/Masters/mWishlist.cs
@@ -4,7 +4,19 @@ namespace Sahakaar_API.Models.Masters
 {
     public class mWishlistAdd
     {
+        private decimal _itemDesignId = 0;
+
         [Required(ErrorMessage = "ItemDesignId is required.")]
-        public decimal F_ItemDesignMaster { get; set; }
+        public decimal F_ItemDesignMaster
+        {
+            get => _itemDesignId;
+            set => _itemDesignId = value;
+        }
+
+        public decimal ItemDesignId
+        {
+            get => _itemDesignId;
+            set => _itemDesignId = value;
+        }
     }
 }
