ab3dc26270ae91c4b38f3f77cdc170bed5fc9b96
kuljeet007 <kuljeetsingh01588@gmail.com>
2026-07-25 15:56:20 +0530

    Execute direct DELETE command for BarcodeTemplateMaster in ctlMaster

--- Sahakaar_API/Controllers/V1/Masters/ctlMaster.cs
+++ Sahakaar_API/Controllers/V1/Masters/ctlMaster.cs
@@ -247,13 +247,10 @@ namespace Sahakaar_API.Controllers.V1.Masters
                 }
                 else if (TableName == "BarcodeTemplateMaster")
                 {
-                    var response = await _svc.Delete(Id, TableName);
-                    if (response > 0 || response == -1)
-                    {
-                        return Ok(new Response { Success = true, Status = StatusCodes.Status200OK, Message = "Record deleted.", Data = new { response } });
-                    }
-
-                    return NotFound(new Response { Success = false, Status = StatusCodes.Status404NotFound, Message = "Not Found" });
+                    var dbPara = new DynamicParameters();
+                    dbPara.Add("Id", Id, DbType.Decimal);
+                    var response = _svc._dapperManager.Execute("DELETE FROM BarcodeTemplateMaster WHERE Id = @Id", dbPara, CommandType.Text);
+                    return Ok(new Response { Success = true, Status = StatusCodes.Status200OK, Message = "Record deleted.", Data = new { response } });
                 }
                 else
                 {
